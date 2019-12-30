d932b96e279249249f55642ccf09cabeacb24598
Kimberly Anne Chua <45088930+potetz@users.noreply.github.com>
2019-12-30 19:43:35 +0800

    fixed

--- tanong_db/urls.py
+++ tanong_db/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import include, path
+
 urlpatterns = [
-    # path('', )
-    path('login/', views.login)
+    path('admin/', admin.site.urls),
+    path('', include('database.urls'))
 ]
