0eaba9f438b462d4f0c5634d299de671c7a298d0
Kevin Sibug <38392797+kedansi@users.noreply.github.com>
2020-02-26 19:29:29 +0800

    Added Create Student form and view

---
+++ database/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Student
+
+
+class NewStudentForm(forms.ModelForm):
+    class Meta:
+        model = Student     
+        fields = ['student_id', 'student_first_name', 'student_middle_name',
+                  'student_last_name', 'student_birthdate', 'student_age', 'student_deped_lrd']
+        
--- tanong_db/urls.py
+++ tanong_db/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from database.views import student_form_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('database.urls'))
+    path('', include('database.urls')),
+    path('createstudent/', student_form_view),
 ]
