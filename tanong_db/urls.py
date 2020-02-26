ffd81691c070411211ca2f26e60d14ff6d57d1ad
Kevin Sibug <38392797+kedansi@users.noreply.github.com>
2020-02-26 19:48:56 +0800

    Revert "Added Create Student form and view"
    This reverts commit 0eaba9f438b462d4f0c5634d299de671c7a298d0.

--- tanong_db/urls.py
+++ tanong_db/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from database.views import student_form_view
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('database.urls')),
-    path('createstudent/', student_form_view),
+    path('', include('database.urls'))
 ]
