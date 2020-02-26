9956e7efe2620fc0c4e86f19cde9a14121b1f10d
Kevin Sibug <38392797+kedansi@users.noreply.github.com>
2020-02-26 20:36:31 +0800

    Added Student Form and View

--- database/views.py
+++ database/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .forms import NewStudentForm
+from .models import Student
 
 # Create your views here.
+def create_student_view(request):
+    form = NewStudentForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = NewStudentForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'new_student_form.html', context)
--- tanong_db/urls.py
+++ tanong_db/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from database.views import create_student_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('database.urls'))
+    path('', include('database.urls')),
+    path('createstudent/', create_student_view),
 ]
