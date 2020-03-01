0be1a16708eaafa342b5d24bb3d9bb0fa744aeec
YaoMorgan <morgan.yao@obf.ateneo.edu>
2020-03-01 16:44:44 +0800

    First pull request changes from models

--- database/models.py
+++ database/models.py
@@ -1,61 +1,58 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
 class Request(models.Model):
-    request_ID = models.IntegerField(max_length=50)
-    user_access_ID= models.CharField(max_length=250)
-    request_time = models.DateTimeField(max_length=100)
+    user_access_ID= models.CharField()
+    request_time = models.DateTimeField(max_length=100,auto_now_add=True)
 
-class User(models.Model):
-    user_id = models.IntegerField(max_length=50)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=250)
-    user_type = models.CharField(max_length=20)
+class User(models.User):
+    # email = models.EmailField(max_length=100)
+    # password = models.CharField()
+    user_type = models.CharField()
 
-class Adviser(models.Model):
-    adviser_id = models.IntegerField(max_length=50)
-    full_name = models.CharField(max_length=250)
-    email_address = models.EmailField(max_length=100)
-    password = models.CharField(max_length=250)
+class Adviser(models.User):
+    first_name = models.CharField()
+    middle_name = models.CharField()
+    last_name = models.CharField()
+    User=models.OnetoOneField(User)
 
 class Section(models.Model):
-    section_id = models.IntegerField(max_length=50)
-    year_level = models.CharField(max_length=10)
-    name = models.CharField(max_length=20)
+    year_level = models.IntegerField(max_length=5)
+    name = models.CharField()
+    Adviser = models.ManytoManyField(Adviser)
+    schoolyear = models.ForeignKey(SchoolYear, on_delete=models.CASCADE,blank='false',null='false')
 
-class Schoolyear(models.Model):
-    schoolyear_id = models.IntegerField(max_length=50)
-    years = models.CharField(max_length=10)
+class SchoolYear(models.Model):
+    years = models.CharField()
 
 class Student(models.Model):
-    student_id = models.IntegerField(max_length=10)
-    full_name = models.CharField(max_length=250)
-    birthdate = models.DateTimeField()
-    sex = models.CharField(max_length=10)
-    LRN = models.IntegerField(max_length=10, blank='true', null='true')
-    age = models.IntegerField(max_length=3)
+    first_name = models.CharField()
+    middle_name = models.CharField()
+    last_name = models.CharField()
+    birthdate = models.DateField()
+    sex_choices =[(MALE,'male'),(FEMALE,'female')]
+    sex = models.CharField(choices=gender_choices,default=null)
+    LRN = models.IntegerField(max_length=10, blank=True, null=True)
 
 class Enrollment(models.Model):
-    enrollment_id = models.IntegerField(max_length=100)
     quarter = models.IntegerField(max_length=1)
     school_days = models.IntegerField(max_length=3)
     days_absent = models.IntegerField(max_length=3)
-    days_present = models.IntegerField(max_length=3)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE, blank='false', null="false")
-    section_id = models.ForeignKey(Section, on_delete=models.CASCADE, blank='false', null="false")
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    section_id = models.ForeignKey(Section, on_delete=models.CASCADE)
 
 class Grade(models.Model):
-    grade_id = models.IntegerField(max_length=10)
-    student_id = models.ForeignKey(Student, on_delete=models.CASCADE, blank='false', null='false')
-    subject_id = models.ForeignKey(subject, on_delete=models.CASCADE, blank='false', null='false')
-    remark = models.CharField(max_length=250,blank='true',null='true')
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    numerical_grade = models.IntegerField()
+    remark = models.CharField(blank='true',null='true')
 
 class Subject(models.Model):
-    subject_id = models.IntegerField(max_length=10)
-    name = models.CharField(max_length=50)
-    subject_type = models.CharField(max_length=10)
+    name = models.CharField()
+    subject_type = models.CharField()
 
 class CoreValues(models.Model):
-    corevalue_id = models.IntegerField(max_length=10)
-    behavior_statement = models.CharField(max_length=300)
-    mark = models.CharField(max_length=2)
+    behavior_statement = models.CharField()
+    mark = models.CharField()
